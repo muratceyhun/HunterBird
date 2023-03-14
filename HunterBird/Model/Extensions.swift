10066b2ab67e74f2ca1587151068a2968378738e
Murat Ceyhun KORPEOGLU <muratceyhunkorpeoglu@gmail.com>
2023-03-14 23:20:52 +0300

    updates

--- HunterBird/Model/Extensions.swift
+++ HunterBird/Model/Extensions.swift
@@ -12,7 +12,6 @@ import SpriteKit
 
 extension CGPoint {
     
-    
     static public func +(left : CGPoint, right : CGPoint) -> CGPoint {
         return CGPoint(x: left.x+right.x, y: left.y+right.y)
     }
@@ -22,8 +21,6 @@ extension CGPoint {
     static public func *(left : CGPoint, right : CGFloat) -> CGPoint {
         return CGPoint(x: left.x*right, y: left.y*right)
     }
-    
-    
 }
 
 
